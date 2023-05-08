5d397de443afbda71e1c75443f61f284a3522fc4
R. Gyeul <wingd4705@gmail.com>
2023-05-08 14:10:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function() {
   function changeImage() {
     var BackgroundImg = [
-      "/Assets/backgrounds/team01.jpg",
-      "/Assets/backgrounds/team02.jpg",
-      "/Assets/backgrounds/team03.jpg",
-      "/Assets/backgrounds/team04.jpg"
+      "Assets/backgrounds/team01.jpg",
+      "Assets/backgrounds/team02.jpg",
+      "Assets/backgrounds/team03.jpg",
+      "Assets/backgrounds/team04.jpg"
     ];
     var i = Math.floor(Math.random() * 4);
     document.getElementById("home").style.backgroundImage =
